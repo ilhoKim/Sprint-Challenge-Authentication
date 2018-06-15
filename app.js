e5a566a668c0b8665f66d94a2605ac68597b5315
Ilho Kim <ilhokim@Ilhoui-MacBook-Pro.local>
2018-06-15 16:15:10 -0700

    Typos

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 5000;
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/auth-users', {
   useMongoClient: true
-});
+  });
 
 server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
